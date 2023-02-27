3f8a992fbad39f5b74fd2410ad96917bec8ed230
J0K3Rn <JosephKeeganRichardson@gmail.com>
2023-02-26 20:06:09 -0600

    Adding database and deployable site

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Blog-Website
  
-Blog with dynamically generated web templates for each post. Site is initially blank, create posts by add to /compose to the url
+Blog Website created with node, express, ejs, and MongoDB
+
+Ready to be deployed using MongoDB Atlas and Heroku
 
 Features:
 - Dynamically generated pages/urls
@@ -8,10 +10,12 @@ Features:
 - Create new dynamic pages
 
 How to run application:
+- Start a MongoDB Shell
 - Download repository
 - Open downloaded repository with a command line interface
 - Run 'npm install' and then 'nodemon app.js' in terminal in root directory of repository
 - Visit the site by opening a browser and going to the url: 'localhost:3000'
+- Create new posts by going to the url: 'localhost:3000/compose'
 
 Homepage:
 
--- app.js
+++ app.js
@@ -5,12 +5,48 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
+
+// Database libraries
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+// Database Setup
+if (process.env.PORT === undefined) {
+  // Local Database connection
+  mongoose.connect('mongodb://localhost:27017/blogWebsiteDB');
+} else {
+  // Cloud Database connection
+  let username = "";
+  let password = "";
+  let clusername = "";
+  mongoose.connect('mongodb+srv://' + username + ':' + password + '@' + clusername + '.mongodb.net/blogWebsiteDB');
+}
+
+// Database Schema's
+const postSchema = new mongoose.Schema({
+  postTitle: {
+    type: String,
+    required: [true, "Title for post is required!"]
+  },
+  postBody: {
+    type: String,
+    required: [true, "Post must have content!"]
+  }
+});
+const Post = mongoose.model("Post", postSchema);
+
+// Default posts
+const post1 = new Post({
+  postTitle: 'Getting Started',
+  postBody: 'Welcome to your todolist! Add more posts by heading to /compose! --> http://localhost:3000/compose'
+});
+
+const defaultPosts = [post1];
+
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const blogPosts = [];
-let postIDGenerator = 0;
 
 const app = express();
 
@@ -26,20 +62,37 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  const post = {
-    title: req.body.newPostTitle,
-    body: req.body.newPostBody,
-    postID: postIDGenerator++
-  };
-  blogPosts.push(post);
+
+  const post = new Post({
+    postTitle: req.body.newPostTitle,
+    postBody: req.body.newPostBody
+  });
+  post.save();
 
   res.redirect("/");
 });
 
 app.get("/home", function (req, res) {
-  res.render("home", {
-    content: homeStartingContent,
-    posts: blogPosts
+  Post.find({}, function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (posts.length === 0) {
+        Post.insertMany(defaultPosts, function (error, items) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Successfully saved default posts to database!");
+            res.redirect("/");
+          }
+        });
+      } else {
+        res.render("home", {
+          content: homeStartingContent,
+          posts: posts
+        });
+      }
+    }
   });
 });
 
@@ -62,29 +115,38 @@ app.get("/compose", function (req, res) {
 // Dynamic URL
 app.get("/posts/:post", function (req, res) {
   //console.log(req.params.post);
-  let found = false;
 
   // Search by post title or post ID
-  blogPosts.forEach(function (element) {
-    if (_.lowerCase(element.postID) === _.lowerCase(req.params.post) || _.lowerCase(element.title) === _.lowerCase(req.params.post)) {
+  Post.findOne({
+    $or: [{
+      '_id': req.params.post
+    }, {
+      postTitle: req.params.post
+    }]
+  }, function (err, post) {
+    if (err) {
+      console.log("Post Not Found!");
+      console.log(err);
+      res.redirect("/");
+    } else {
       console.log("Post Found!");
-      found = true;
       res.render("post", {
-        post: element
+        post: post
       });
     }
-  });
 
-  if (!found) {
-    console.log("Post Not Found!");
-    res.redirect("/");
-  }
+  });
 });
 
 app.get('*', function (req, res) {
   res.status(404).send("You're not supposed to be here!");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log(process.env.PORT);
+  if (process.env.PORT === undefined) {
+    console.log("Server is running on port 3000.");
+  } else {
+    console.log("Server is running on port " + process.env.PORT + ".");
+  }
 });
